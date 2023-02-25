2bec5e6682d393d513fdb91f4f15a3af9380ceae
pawankr <pawanchandra9001@gmail.com>
2023-02-25 11:54:31 +0530

    Initial front-end project creation

---
+++ NeerajHostel-FrontEnd/Controllers/UserController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using NeerajHostel_FrontEnd.Models;
+
+namespace NeerajHostel_FrontEnd.Controllers
+{
+    public class UserController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        [HttpGet]
+        public IActionResult Signin()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Signin(User args)
+        {
+            return View();
+        }
+    }
+}
---
+++ NeerajHostel-FrontEnd/Models/User.cs
@@ -0,0 +1,12 @@
+﻿namespace NeerajHostel_FrontEnd.Models
+{
+    public class User
+    {
+        public Guid Id { get; set; }
+        public string? Name { get; set; }
+        public string Email { get; set; }
+        public string? Contact { get; set; }
+        public string Password { get; set; }
+        public string ConfirmPassword { get; set; }
+    }
+}
